f61d063881c5cab081c0bf2d934e7ab07b70c118
Papitux <papitux.br@gmail.com>
2015-07-23 19:06:41 -0300

    antlr4-451

---
+++ antlr4-451/etc/antlr4.csh
@@ -0,0 +1,2 @@
+#!/bin/csh
+setenv CLASSPATH /usr/share/java/antlr4.jar:$CLASSPATH
---
+++ antlr4-451/etc/antlr4.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+#alias antlr4='java -Xmx500M -cp "/usr/share/java/antlr4.jar" org.antlr.v4.Tool'
+#alias grun='java org.antlr.v4.runtime.gui.TestRig'
+export CLASSPATH="/usr/share/java/antlr4.jar:$CLASSPATH"
